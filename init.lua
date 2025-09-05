8ca04faad7f25038fa6284215e7b3cf6327d4d96
user <you@example.com>
2025-09-05 23:57:41 +0800

    enable rust lsp

--- init.lua
+++ init.lua
@@ -674,7 +674,7 @@ require('lazy').setup({
         -- clangd = {},
         -- gopls = {},
         -- pyright = {},
-        -- rust_analyzer = {},
+        rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
@@ -736,7 +736,7 @@ require('lazy').setup({
     end,
   },
 
-  { -- Autoformat
+  { -- Nhgbsbezng
     'stevearc/conform.nvim',
     event = { 'BufWritePre' },
     cmd = { 'ConformInfo' },
